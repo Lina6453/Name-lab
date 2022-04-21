e1eabc49cf4b4e4fb4aea5e2aac9474c713d43e1
Lina6453 <angel.gabid@yandex.ru>
2022-04-21 22:42:58 +0300

    My second pull recuest

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Имя
         string name;
         const double k = 1.5;
         string age;
+        const double x = 3;
         public Person(string nm)
         {
             name = nm;
@@ -29,6 +30,11 @@ namespace Имя
         public int Dohod(int p)
         {
             return Convert.ToInt32(Math.Abs(p * k));
+        }
+
+        public int MaxDohod (int p)
+        {
+            return Convert.ToInt32(Math.Abs(p * x));
         }
 
     }
@@ -54,6 +60,8 @@ namespace Имя
             Console.WriteLine( "Имя=  " + q.GetName());
              Person n = new Person(u);
             Console.WriteLine("Доход=  " + n.Dohod(f).ToString());
+            Person b = new Person(u)
+            Console.WriteLine("Максимальный доход=" + b.MaxDohod(f).ToString());
             Console.ReadKey();
         }
     }
